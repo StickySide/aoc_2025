7d4824b2a2a5056330450030b6b3b5b7d4c98f76
Jon <jnorton6@gmail.com>
2025-12-02 12:20:35 -0500

    Added comments and slight refactor for Day 2 Part 2

--- day02/main.py
+++ day02/main.py
@@ -1,3 +1,6 @@
+# Advent of code 2025 Day 2 Part 2
+
+
 def get_input(filename: str) -> list[str]:
     input = ""
     with open(filename, "r") as input_file:
@@ -6,16 +9,25 @@ def get_input(filename: str) -> list[str]:
 
 
 def make_range_list(id_range_string: str) -> list[str]:
+    """
+    From the string format "xxx-yyy" return a list of strings from xxx to yyy
+    """
     start, end = id_range_string.split("-")
     return [str(n) for n in range(int(start), int(end) + 1)]
 
 
 def is_invalid_id(id: str) -> bool:
+    """
+    Return if an id number is invalid (has repetition twice or more)
+    """
     search_string = (id + id)[1:-1]
     return id in search_string
 
 
 def get_invalid_ids(id_range: str) -> list[int]:
+    """
+    Return a list of invalid IDs from an id range
+    """
     invalid_ids: list[int] = []
     ids = make_range_list(id_range)
     for id in ids:
@@ -25,13 +37,22 @@ def get_invalid_ids(id_range: str) -> list[int]:
 
 
 def total_invalid_ids(input: list[str]) -> int:
+    """
+    Return a the sum total of all the invalid ids found in the input file
+    """
     total: int = 0
     for entry in input:
-        for invalid_id in get_invalid_ids(entry):
+        invalid_ids = get_invalid_ids(entry)
+        print(
+            f"Checking {entry}: {len(invalid_ids) if len(invalid_ids) > 0 else 'No'} invalid IDs found -"
+        )
+        for id in invalid_ids:
+            print(f"    {id}")
+        for invalid_id in invalid_ids:
             total += invalid_id
     return total
 
 
 if __name__ == "__main__":
     input = get_input("input.txt")
-    print(total_invalid_ids(input))
+    print(f"Sum of all invalid IDs: {total_invalid_ids(input)}")

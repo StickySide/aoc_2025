dcc2553593cf686fdf69d3534740d92be3be87bf
Jon <jnorton6@gmail.com>
2025-12-19 10:26:57 -0500

    Added distance calculation and produced a sorted list of all combinations day 8 part 1

--- day08/part1.py
+++ day08/part1.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from itertools import combinations
 
 coord = tuple[int, int, int]
 
@@ -10,11 +11,18 @@ def get_input(filename: str) -> list[coord]:
         ]
 
 
-def distance(p: coord, q: coord) -> float:
+def distance(pair: tuple[coord, coord]) -> float:
+    p = pair[0]
+    q = pair[1]
     return sqrt((p[0] - q[0]) ** 2 + (p[1] - q[1]) ** 2 + (p[2] - q[2]) ** 2)
 
 
 if __name__ == "__main__":
     data = get_input("testinput.txt")
-    print(data)
-    print(distance(data[0], data[1]))
+
+    combos: list[tuple[coord, coord]] = [combo for combo in combinations(data, 2)]
+
+    combos.sort(key=lambda x: distance(x))
+
+    for combo in combos:
+        print(combo)
